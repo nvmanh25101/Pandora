8086b9b6bd58d3e2478aeb7a5098dc20a72407e7
Manh <nvmanh25101@gmail.com>
2023-04-15 10:48:26 +0700

    update user: process sigin

--- process-signin.php
+++ process-signin.php
@@ -1,32 +1,38 @@
 <?php
-    session_start();
-    if(isset($_POST['btnSignin'])){
-        $email = $_POST['email'];
-        $pass = $_POST['password']; 
-        require './database/connect.php';
+session_start();
+if (isset($_POST['btnSignin'])) {
+    $email = $_POST['email'];
+    $pass = $_POST['password'];
+    require './database/connect.php';
 
-        $sql = "SELECT * FROM users where email = '$email'";
-        $result = mysqli_query($connect,$sql);
-        if(mysqli_num_rows($result) > 0){
-            $row = mysqli_fetch_assoc($result);
-            $id = $row['id'];
-            $name = $row['name'];
-            $pass_hash = $row['password'];
-            if(password_verify($pass,$pass_hash)){
+    $sql = "SELECT * FROM users where email = '$email'";
+    $result = mysqli_query($connect, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        $id = $row['id'];
+        $name = $row['name'];
+        $pass_hash = $row['password'];
+        if ($row['status'] == 1) {
+            if (password_verify($pass, $pass_hash)) {
                 $_SESSION['id'] = $id;
                 $_SESSION['name'] = $name;
                 header("location:index.php");
-            }else{
+            } else {
                 $error = "Incorrect account or password ";
                 header("location:signin.php?error=$error");
             }
         }
-        else{
+        else {
+            $error = "Kiểm tra email của bạn để xác thực tài khoản!";
+            header("location:signin.php?error=$error");
+        }
+    }
+    else{
             $error = "Incorrect account or password ";
             header("location:signin.php?error=$error");
         }
-    }else{
+    } else {
         header("location: signin.php");
     }
-    
-?>
+
+    ?>
